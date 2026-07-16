364ee31ec30b67b914c86b95adfa4c88759d0e26
Bohdan Holovchak <you@example.com>
2026-07-16 17:28:22 +0300

    CL-15 further example microservice

--- cleanup/app/app.go
+++ cleanup/app/app.go
@@ -1,31 +1,63 @@
 package application
 
 import (
-	"net/http"
-	"fmt"
 	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
 }
 
-func (a *App) Start(ctx context.Context) error{
+func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr: ":5000",
+		Addr:    ":5000",
 		Handler: a.router,
 	}
-	err := server.ListenAndServe()
+
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
-	return nil
+
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("Starting Server!")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
+	}
+
 }
--- cleanup/main.go
+++ cleanup/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	application "github.com/CommandLine-5336/Supernatural/cleanup/app"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Printf("Error starting the application: %v\n", err)
 	}
