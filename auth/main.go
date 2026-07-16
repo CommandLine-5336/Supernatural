136bf87d66dc97320bb6044bc9f8d354f5de1f31
Maksym Semehen <semehen.devops@proton.me>
2026-07-16 19:39:09 +0300

    chore(auth):CL-21 draft endpoint structures

--- auth/main.go
+++ auth/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"net/http"
+)
+
 type Login struct {
 	HashedPassword string
 	SessionToken   string
@@ -7,3 +11,19 @@ type Login struct {
 }
 
 var users = map[string]Login{}
+
+func main() {
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/home", home)
+	http.ListenAndServe(":8080", nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {}
+
+func login(w http.ResponseWriter, r *http.Request) {}
+
+func logout(w http.ResponseWriter, r *http.Request) {}
+
+func home(w http.ResponseWriter, r *http.Request) {}
