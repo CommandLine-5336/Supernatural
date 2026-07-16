dfe3e80cb0461023cc01d3ab6c59c6ae0f7a0835
Bohdan Holovchak <you@example.com>
2026-07-16 16:47:14 +0300

    CL-15 Trying out example GO application

--- cleanup/main.go
+++ cleanup/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/CommandLine-5336/Supernatural/cleanup/app"
+
+	application "github.com/CommandLine-5336/Supernatural/cleanup/app"
 )
 
 func main() {
- app := application.New()
- err := app.Start(context.TODO())
- if err != nil {
-	fmt.Printf("Error starting the application:", err)
- }
+	app := application.New()
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Printf("Error starting the application:", err)
+	}
 }
