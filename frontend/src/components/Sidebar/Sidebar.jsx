e975197b80c09d3f9b7466d0a91d4a9691245dbb
Marta Kozak <martakozak94666@gmail.com>
2026-07-20 22:07:53 +0300

    Change frontend styling

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -6,12 +6,22 @@ export default function Sidebar({ user, onLogout }) {
   return (
     <aside className="sidebar">
       <div className="sidebar__top">
-        <div className="sidebar__user-pill">
-          <span className={`sidebar__avatar sidebar__avatar--${user.level}`} />
-          <span className="sidebar__username">{user.displayName}</span>
+        <div className="sidebar__header-row">
+          <div className="sidebar__user-pill">
+            <span className={`sidebar__avatar sidebar__avatar--${user.level}`} />
+            <span className="sidebar__username">{user.displayName}</span>
+          </div>
+          <LogoutButton onClick={onLogout} />
         </div>
 
-        <LogoutButton onClick={onLogout} />
+        <div className="sidebar__card">
+          <p className="sidebar__card-text">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. 
+          In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. 
+          Iaculis massa nisl malesuada lacinia integer nunc posuere. 
+          Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+          </p>
+        </div>
       </div>
     </aside>
   );
--- frontend/src/data/mapConfig.js
+++ frontend/src/data/mapConfig.js
@@ -1,4 +1,4 @@
 
 export const DEFAULT_CENTER = [49.8397, 24.0297];
-export const DEFAULT_ZOOM = 12;
-export const MAP_TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+export const DEFAULT_ZOOM = 4;
+export const MAP_TILE_URL = "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png";
