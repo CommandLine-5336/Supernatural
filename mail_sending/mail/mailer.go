5632f86127e5c9530207b9a5d1abc57f440b28b2
Denis <denisfolyush@gmail.com>
2026-07-22 17:27:11 +0300

    feat(mailing):CL-19 now data fetch from db, in email we have user status and alies

--- mail_sending/mail/mailer.go
+++ mail_sending/mail/mailer.go
@@ -9,41 +9,42 @@ import (
 	"gopkg.in/gomail.v2"
 )
 
-func Mail_sender() error {
+func SendPasswordMail(emailAddress string, userStatus string, userAlias string) error {
 	email_host := os.Getenv("EMAIL_HOST")
 	email_from := os.Getenv("EMAIL_FROM")
 	password := os.Getenv("EMAIL_PASSWORD")
+	var AWSpassword = "ab&uOJ^ILDgg,dslOD" //this part need to be done via AWS secretmager
+	templateData := struct {
+		Status     string
+		SecretPswd string
+		Alias      string
+	}{
+		Status:     userStatus,
+		SecretPswd: AWSpassword,
+		Alias:      userAlias,
+	}
 
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		panic(fmt.Errorf("failed to parse template file: %w", err))
 	}
-	students := map[int]string{
-		101: "denisfolyush@gmail.com",
-		102: "soosysoosy69@gmail.com",
-		// 103: "enisfolyush@gmail.com",
-	}
+
 	d := gomail.NewDialer(email_host, 587, email_from, password)
 
-	for _, value := range students {
-		var bodyBuffer bytes.Buffer
-		if err := tmpl.Execute(&bodyBuffer, nil); err != nil {
-			return err
-		}
+	var bodyBuffer bytes.Buffer
+	if err := tmpl.Execute(&bodyBuffer, templateData); err != nil {
+		return err
+	}
 
-		m := gomail.NewMessage()
+	m := gomail.NewMessage()
+	m.SetHeader("From", "unown@gmail.com")
+	m.SetHeader("To", emailAddress)
+	m.SetHeader("Subject", "Weather Forecast for this night!")
+	m.SetBody("text/html", bodyBuffer.String())
 
-		m.SetHeader("From", "unown@gmail.com")
-		m.SetHeader("To", value)
-		// m.SetAddressHeader("Cc", "denisfolyush@gmail.com", "Daen")
-		m.SetHeader("Subject", "Weather Forecast for this night!")
-		m.SetBody("text/html", bodyBuffer.String())
-		// m.Attach("/home/Alex/lolcat.jpg")
-
-		// Send the email to Bob, Cora and Dan.
-		if err := d.DialAndSend(m); err != nil {
-			return err
-		}
+	if err := d.DialAndSend(m); err != nil {
+		return err
 	}
 	return nil
+
 }
--- mail_sending/main.go
+++ mail_sending/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"mail_sending/config"
 	"mail_sending/mail"
@@ -8,10 +9,17 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type User struct {
+	ID     uint `gorm:"primaryKey"`
+	Alias  string
+	Email  string
+	Status string
+}
+
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error .env not finded")
+		log.Fatal("Error .env not found")
 	}
 }
 
@@ -21,8 +29,23 @@ func main() {
 		log.Fatal("Error db not connected")
 
 	}
+	var users []User
 
-	err = mail.Mail_sender()
+	result := config.DB.Find(&users)
+	if result.Error != nil {
+		log.Fatal("Error fetchin emails")
+
+	}
+	// send email for each email in db
+	for _, user := range users {
+		fmt.Printf("Here is our users %s his status is %s\n", user.Email, user.Status)
+		err = mail.SendPasswordMail(user.Email, user.Status, user.Alias)
+		if err != nil {
+			log.Fatalf("email not send", err)
+
+		}
+	}
+	// err = mail.Mail_sender()
 	if err != nil {
 		log.Fatal("Error sending emails")
 
