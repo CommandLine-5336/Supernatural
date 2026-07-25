f7a55fc2bb0458e824227653ea25c13d4c0cf08e
Marta Kozak <martakozak94666@gmail.com>
2026-07-25 15:24:10 +0300

    Fix duplicates in settings.py

--- general/core/settings.py
+++ general/core/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -80,7 +79,11 @@ DATABASES = {
     }
 }
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8080", "http://127.0.0.1:8080"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://localhost:5173",
+]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080", "http://127.0.0.1:8080"]
 
@@ -109,10 +112,6 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
 
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
