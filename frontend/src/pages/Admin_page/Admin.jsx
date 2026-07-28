5a519d00b6a5f234cb714ec55dedeb9699f74bcb
Bohdan Holovchak <you@example.com>
2026-07-28 16:19:29 +0300

    feature(React):CL-41 Added COMPROMISED button and fixed sidebar clickable for coppers

--- frontend/src/pages/Admin_page/Admin.jsx
+++ frontend/src/pages/Admin_page/Admin.jsx
@@ -1,24 +1,40 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import BackButton from '../../shared/ui/BackButton/BackButton';
 import "./Admin.css";
+import EraseButton from '../../shared/ui/EraseButton/EraseButton';
+import { erase } from '../../api/erase';
 
-export default function Admin() {
+export default function Admin({ user }) {
   const navigate = useNavigate();
+  const [eraseMessage, setEraseMessage] = useState("");
+
+  const handleErase = async () => {
+    const confirmed = window.confirm("This will permanently erase all data. Are you sure?");
+    if (!confirmed) return;
+    const response = await erase();
+    const data = await response.json();
+    setEraseMessage(data.message);
+    navigate('/')
+  };
+
   return (
 <div>
         <header className="admin-header">
             <div className="admin-header-row">
               <BackButton onClick={() => window.location.href = '/'} style={{ margin: '60px' }}/>
-              <h1 className="admin-title">Admin pref</h1>
+              <h1 className="admin-title">Admin</h1>
             </div>
         </header>
-      <div className="mail-container">
+      <div className="admin-container" style={{ display: ["gold", "silver"].includes(user?.status) ? "flex" : "none" }}>
       <button
           type="button"
           className="post-form__submit"
           onClick={() => navigate('/mail')}>
           Create mails
         </button>
+        <EraseButton onClick={handleErase} style={{ display: user?.status === "gold" ? "flex" : "none" }}/>
+        {eraseMessage && <p>{eraseMessage}</p>}
       </div>
   </div>
   );
