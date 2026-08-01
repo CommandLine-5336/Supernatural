01bb2d06116e50246beab5263e06fe2021af9a8a
Maksym Semehen <semehen.devops@proton.me>
2026-08-01 18:30:36 +0300

    refactor: CL-42 simplify password generation

--- password_generator/main.go
+++ password_generator/main.go
@@ -93,41 +93,21 @@ func rotatePassword(db *sql.DB) error {
 }
 
 func PasswordGenerator(passwordLength int) string {
-	// Character sets for generating passwords
-	lowerCase := "abcdefghijklmnopqrstuvwxyz"
-	upperCase := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	numbers := "0123456789"
-	specialChar := "!@#$%^&*()_-+={}[/?]"
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 	// Variable for storing password
-	password := ""
+	password := make([]byte, passwordLength)
 
 	// Initialize the random number generator
 	source := rand.NewSource(time.Now().UnixNano())
 	rng := rand.New(source)
 
 	// Generate password character by character
-	for n := 0; n < passwordLength; n++ {
-		// Generate a random number to choose a character set
-		randNum := rng.Intn(4)
-
-		switch randNum {
-		case 0:
-			randCharNum := rng.Intn(len(lowerCase))
-			password += string(lowerCase[randCharNum])
-		case 1:
-			randCharNum := rng.Intn(len(upperCase))
-			password += string(upperCase[randCharNum])
-		case 2:
-			randCharNum := rng.Intn(len(numbers))
-			password += string(numbers[randCharNum])
-		case 3:
-			randCharNum := rng.Intn(len(specialChar))
-			password += string(specialChar[randCharNum])
-		}
+	for i := range password {
+		password[i] = chars[rng.Intn(len(chars))]
 	}
 
-	return password
+	return string(password)
 }
 
 func sendPassword(password string) error {
