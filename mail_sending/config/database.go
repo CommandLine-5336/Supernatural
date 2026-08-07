cadf8af5c40d2a8077e1fff64dde8dc3c6de3b3b
Maksym Semehen <semehen.devops@proton.me>
2026-08-07 16:44:56 +0300

    refactor: CL-68 simplify database connection

--- mail_sending/config/database.go
+++ mail_sending/config/database.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -13,19 +12,9 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() (*gorm.DB, error) {
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASS")
-	dbname := os.Getenv("DB_NAME")
-	host := os.Getenv("DB_HOST")
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		host, user, password, dbname, port,
-	)
-
 	var err error
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
