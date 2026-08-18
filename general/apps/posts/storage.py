12b6136b3da98b7c332f64b4b319b1170618b178
Bohdan Holovchak <you@example.com>
2026-08-18 16:07:11 +0300

    fix:CL-60 removed aws keys from django

--- general/apps/posts/storage.py
+++ general/apps/posts/storage.py
@@ -11,8 +11,6 @@ def _get_s3_client():
     return boto3.client(
         "s3",
         region_name=settings.AWS_REGION,
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
     )
 
 
